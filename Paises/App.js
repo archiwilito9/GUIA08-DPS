e17ae3b673af3ccd8cd8b151ba4542c143908a88
Marvin Tobar <archiwilito9@yahoo.com>
2021-10-23 23:58:43 -0600

    arreglado

--- Paises/App.js
+++ Paises/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import {Node} from 'react';
+import { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,16 +29,16 @@ import {
 
 import Pais from './src/components/pais';
 
-const [busqueda, guardarbusqueda]=useState({
-  pais:''
+const [busqueda, guardarbusqueda] = useState({
+  pais: ''
 })
 
 return (
   <View style={styles.app}>
     <View style={styles.contenido}>
-      <Formulario 
-      busqueda={busqueda}
-      guardarbusqueda={guardarbusqueda}
+      <Formulario
+        busqueda={busqueda}
+        guardarbusqueda={guardarbusqueda}
       ></Formulario>
     </View>
   </View>
@@ -65,52 +65,52 @@ const styles = StyleSheet.create({
 
 export default function App() {
   const [busqueda, guardarbusqueda] = useState({
-  pais: '',
+    pais: '',
   });
   const [consultar, guardarconsultar] = useState(false);
   const [resultado, guardarresultado] = useState({});
   useEffect(() => {
-  const { pais } = busqueda;
-  const consultarPais = async () => {
-  if (consultar) {
-  const url =
-  `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
-  try {
-  const respuesta = await fetch(url);
-  const resultado = await respuesta.json();
-  guardarresultado(resultado);
-  guardarconsultar(false);
-  } catch (error) {
-  mostrarAlerta();
-  }
-  }
-  };
-  consultarPais();
-  },[consultar]);
+    const { pais } = busqueda;
+    const consultarPais = async () => {
+      if (consultar) {
+        const url =
+          `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
+        try {
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+          guardarresultado(resultado);
+          guardarconsultar(false);
+        } catch (error) {
+          mostrarAlerta();
+        }
+      }
+    };
+    consultarPais();
+  }, [consultar]);
   const mostrarAlerta = () => {
-  Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'),
-  [{ Text: 'Ok' }];
+    Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'),
+      [{ Text: 'Ok' }];
   };
   return (
-  <View style={styles.app}>
-  <View style={styles.contenido}>
-  <Formulario
-  busqueda={busqueda}
-  guardarbusqueda={guardarbusqueda}
-  guardarconsultar={guardarconsultar}
-/>
-<Pais resultado={resultado} />
-</View>
-</View>
-);
+    <View style={styles.app}>
+      <View style={styles.contenido}>
+        <Formulario
+          busqueda={busqueda}
+          guardarbusqueda={guardarbusqueda}
+          guardarconsultar={guardarconsultar}
+        />
+        <Pais resultado={resultado} />
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-app: {
-flex: 1,
-backgroundColor: 'rgb(71,149,212)',
-justifyContent: 'center',
-},
-contenido: {
-margin: '2.5%',
-},
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center',
+  },
+  contenido: {
+    margin: '2.5%',
+  },
 });
--- Paises/src/components/Formulario.js
+++ Paises/src/components/Formulario.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Text, TextInput, View, StyleSheet, TouchableWithoutFeedback, Animated, Alert } from 'react-native';
-import {Picker} from '@react-native-community/picker';
-const Formulario=(busqueda, guardarbusqueda)=>{
+import { Picker } from '@react-native-community/picker';
+const Formulario = (busqueda, guardarbusqueda) => {
 
-    const {pais}=busqueda;
+    const { pais } = busqueda;
 
     const [animacionboton] = useState(new Animated.Value(1));
 
-    const consultarPais=()=>{
-        if(pais.trim()===""){
+    const consultarPais = () => {
+        if (pais.trim() === "") {
             mostrarAlerta();
             return;
         }
@@ -16,95 +16,96 @@ const Formulario=(busqueda, guardarbusqueda)=>{
         guardarconsultar(true);
     }
 
-    const mostrarAlerta=()=>{
-        Alert.alert("Error","Debe seleccionar un país"), [{Text:"Entendido"}]
+    const mostrarAlerta = () => {
+        Alert.alert("Error", "Debe seleccionar un país"), [{ Text: "Entendido" }]
     }
 
-    const animacionEntrada=()=>{
-        Animated.spring(animacionboton,{
-            toValue:.9
+    const animacionEntrada = () => {
+        Animated.spring(animacionboton, {
+            toValue: .9
         }).start();
     }
 
-    const animacionSalida=()=>{
-        Animated.spring(animacionboton,{
-            toValue:1
+    const animacionSalida = () => {
+        Animated.spring(animacionboton, {
+            toValue: 1
         }).start();
     }
 
-    const estiloAnimacion={
-        transform:[{
-            scale:animacionboton
+    const estiloAnimacion = {
+        transform: [{
+            scale: animacionboton
         }]
     }
 
-return(
-<>
-<View>
-<View>
-<Text style ={styles.input}>Pais</Text>
-</View>
-<View>
-<Picker 
-selectedValue={pais}
-onValueChange={pais=>guardarbusqueda({...busqueda,pais})}
-style={styles.itempais}>
-<Picker.Item label="--seleccione un pais--" value=""/>
-<Picker.Item label="Canada" value="ca" />
-<Picker.Item label="El Salvador" value="sv"/>
-<Picker.Item label="Guatemala" value="gt"/>
-<Picker.Item label="Honduras" value="hn"/>
-<Picker.Item label="Nicaragua" value="ni"/>
-<Picker.Item label="Panama" value="pa"/>
-<Picker.Item label="Costa Rica" value="cr"/>
-<Picker.Item label="Mexico" value="mx"/>
-<Picker.Item label="Argentina" value="ar"/>
-<Picker.Item label="Estados Unidos" value="us"/>
-<Picker.Item label="Colombia" value="co"/>
-<Picker.Item label="España" value="es"/>
-<Picker.Item label="Peru" value="pe"/>
-</Picker>
-</View>
-<TouchableWithoutFeedback 
-    onPressIn={()=> animacionEntrada()}
-    onPressOut={()=> animacionSalida()}
->
-    <Animated.View style={[styles.btnBuscar, estiloAnimacion]}/>
-<View style={styles.btnBuscar}>
-<Text style={styles.textoBuscar}>Buscar País</Text>
-</View>
-</TouchableWithoutFeedback>
-</View>
-</>
-);
+    return (
+        <>
+            <View>
+                <View>
+                    <Text style={styles.input}>Pais</Text>
+                </View>
+                <View>
+                    <Picker
+                        selectedValue={pais}
+                        onValueChange={pais => guardarbusqueda({ ...busqueda, pais })}
+                        style={styles.itempais}>
+                        <Picker.Item label="--seleccione un pais--" value="" />
+                        <Picker.Item label="Canada" value="ca" />
+                        <Picker.Item label="El Salvador" value="sv" />
+                        <Picker.Item label="Guatemala" value="gt" />
+                        <Picker.Item label="Honduras" value="hn" />
+                        <Picker.Item label="Nicaragua" value="ni" />
+                        <Picker.Item label="Panama" value="pa" />
+                        <Picker.Item label="Costa Rica" value="cr" />
+                        <Picker.Item label="Mexico" value="mx" />
+                        <Picker.Item label="Argentina" value="ar" />
+                        <Picker.Item label="Estados Unidos" value="us" />
+                        <Picker.Item label="Colombia" value="co" />
+                        <Picker.Item label="España" value="es" />
+                        <Picker.Item label="Peru" value="pe" />
+                    </Picker>
+                </View>
+                <TouchableWithoutFeedback
+                    onPressIn={() => animacionEntrada()}
+                    onPressOut={() => animacionSalida()}
+                >
+                    <Animated.View style={[styles.btnBuscar, estiloAnimacion]} />
+                    <View style={styles.btnBuscar}>
+                        <Text style={styles.textoBuscar}>Buscar País</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
+        </>
+    );
 }
 
-const styles= StyleSheet.create({
-    input:{padding:10,
-    height:50,
-    fontSize:20,
-    marginBottom:20,
-    textAlign:'center',
-    color :'#000'
+const styles = StyleSheet.create({
+    input: {
+        padding: 10,
+        height: 50,
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center',
+        color: '#000'
     },
-    itempais:{
-    height:60,
-    backgroundColor:'#fff',
+    itempais: {
+        height: 60,
+        backgroundColor: '#fff',
     },
-    btnBuscar:{
-    marginTop:50,
-    height:50,
-    backgroundColor:'#000',
-    fontSize:20,
-    marginBottom:20,
-    textAlign:'center'
+    btnBuscar: {
+        marginTop: 50,
+        height: 50,
+        backgroundColor: '#000',
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center'
     },
-    textoBuscar:{
-    color:'#fff',
-    fontWeight:'bold',
-    textTransform:'uppercase',
-    textAlign:"center",
-    fontSize:18
-}
+    textoBuscar: {
+        color: '#fff',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        textAlign: "center",
+        fontSize: 18
+    }
 })
 export default Formulario;
